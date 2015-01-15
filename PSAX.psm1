90c57db7fc5ed09d63b32338e76bd61516ff13d6
ebh <alex.dess@ansarada.com>
2015-01-15 22:08:50 +1100

    Initial commit

---
+++ PSAX.psm1
@@ -0,0 +1,10 @@
+Import-Module AWSPowerShell
+
+$moduleRoot = Split-Path -Path $MyInvocation.MyCommand.Path
+
+"$moduleRoot\Functions\*.ps1" |
+Resolve-Path |
+Where-Object { -not ($_.ProviderPath.Contains(".Tests.")) } |
+ForEach-Object { . $_.ProviderPath }
+
+Export-ModuleMember Set-Path, Set-PathAccessRule, Create-LocalUser, Set-NetworkPath
