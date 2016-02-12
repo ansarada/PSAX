462834352244e5458567cbd6021da78c178f3177
ebh <alex.dess@ansarada.com>
2016-02-12 12:16:21 +1100

    Added recursive flag to Set-PathAccessRule

---
+++ Functions/Set-PathAccessRule.Tests.ps1
@@ -0,0 +1,115 @@
+﻿#requires -Version 3 -Modules AWSPowerShell
+#Requires -RunAsAdministrator
+
+$currentDirectory = Split-Path -Parent $PSCommandPath
+$sourceFile = (Split-Path -Leaf $PSCommandPath).Replace(".Tests.", ".")
+. "$currentDirectory\$sourceFile"
+
+Describe "Set-PathAccessRule" {
+
+    $testPath = Join-Path $TestDrive 'somefile.txt'
+    Set-Content $testPath -value "my test text."
+
+    $fakeFilePaths = @($testPath)
+    $fakeDirectoryPath = [System.IO.Path]::GetDirectoryName($testPath)
+    $fakeFilePath = ''
+    $emptyFilePaths = @('', '')
+    $fakeArray = @('a', 'b', 'c')
+    $fakehash =  @{"FullControl" = "Allow"}
+    $Username = "unittestuser"
+
+    Context "Testing parameters validation" {
+
+        It "Passing mandatory parameter as null and empty should throw exception" {
+            
+            { Set-PathAccessRule -Path $null -Principles $null -Permissions $null } | Should Throw
+            { Set-PathAccessRule -Path "" -Principles "" -Permissions "" } | Should Throw
+            { $emptyFilePaths | Set-PathAccessRule -Principles $emptyFilePaths -Permissions $emptyFilePaths } | Should Throw
+            { Set-PathAccessRule -Path $fakeFilePath -Principles $fakeArray -Permissions $fakehash } | Should Throw 
+
+        }
+
+        It "When passing a file path instead of a directory path, this method should return exception" {
+
+            { Set-PathAccessRule -Path $testPath -Principles $fakeArray -Permissions $fakehash } | Should Throw
+
+        }
+
+        It "When Get-Acl returns exception this method should return exception" {
+
+            Mock -CommandName Get-Acl -MockWith {throw} 
+
+            { Set-PathAccessRule -Path $fakeDirectoryPath -Principles $fakeArray -Permissions $fakehash } | Should Throw
+
+        }
+
+        It "When Get-Acl returns null this method should return exception" {
+
+            Mock -CommandName Get-Acl -MockWith {return $null} 
+
+            { Set-PathAccessRule -Path $fakeDirectoryPath -Principles $fakeArray -Permissions $fakehash } | Should Throw
+
+        }
+
+
+    }
+    
+
+    Context "Passing real values and testing actual outputs" {
+
+         BeforeEach {
+            Write-Host "Real stuff context begin"
+            $ADSIComp = [adsi]"WinNT://." 
+            $NewUser = $ADSIComp.Create('User',$Username) 
+            $NewUser.SetPassword("P@ssword1")
+            $NewUser.setinfo()
+        }
+
+        AfterEach {
+            Write-Host "Real stuff context ends"
+
+            $ADSIComp = [adsi]"WinNT://."
+            $ADSIComp.Delete('User',$Username) 
+        }
+
+
+        It "Passing all correct values and checking if the return value is as per expectation (not passing recursion switch)" {
+
+            $OperationDetailInfo = Set-PathAccessRule -Path $fakeDirectoryPath -Principles $Username -Permissions $fakehash -PassThru
+            $OperationDetailInfo.TopLevelACL | where {$_.IdentityReference -like "*$Username*"} | Should Not BeNullOrEmpty
+            $OperationDetailInfo.RecursedOpeartionOutput | Should BeNullOrEmpty
+
+        }
+
+        It "Passing all correct values and checking if the return value is as per expectation (passing recursion switch)" {
+
+            $OperationDetailInfo = Set-PathAccessRule -Path $fakeDirectoryPath -Principles $Username -Permissions $fakehash -PassThru -Recurse
+            $OperationDetailInfo.TopLevelACL | where {$_.IdentityReference -like "*$Username*"} | Should Not BeNullOrEmpty
+            $OperationDetailInfo.RecursedOpeartionOutput | Should Match "Successfully processed 1 files;"
+            $OperationDetailInfo.RecursedOpeartionExitCode | Should Be 0
+
+        }
+
+        It "Passing all correct values and checking if passthru parameter is not supplied then the method should not throw exception" {
+
+            {Set-PathAccessRule -Path $fakeDirectoryPath -Principles $Username -Permissions $fakehash} | Should Not throw
+
+        }
+
+        It "When Invoke-Process returns null function throws exception" {
+
+            Mock -CommandName Invoke-Process -MockWith {return $null} 
+            {Set-PathAccessRule -Path $fakeDirectoryPath -Principles $Username -Permissions $fakehash -Recurse} | Should throw "icacls returned null"
+
+        }
+
+        It "When Invoke-Process returns operation status which is unsuccessful function throws exception" {
+
+            Mock -CommandName Invoke-Process -MockWith {return [hashtable]@{"ExitCode"="1"; "ErrorInfo"="Access denied"}} 
+            {Set-PathAccessRule -Path $fakeDirectoryPath -Principles $Username -Permissions $fakehash -Recurse} | Should throw "icacls returned error. Error detail Access denied"
+
+        }
+
+    }
+
+}
--- Functions/Set-PathAccessRule.ps1
+++ Functions/Set-PathAccessRule.ps1
@@ -1,86 +1,182 @@
-﻿<#
+﻿Set-StrictMode -Version Latest
+
+<#
 
 .SYNOPSIS
-	Will set access rule of directory path if it does not exist, otherwise will do nothing
-
-.PARAMETER Path
-	The path to set the access rule
-
-.PARAMETER PRINCIPLE
-	The windows principle that the access rule applies to
-
-.PARAMETER PERMISSIONS
-	The hashtable that contains the file system right (eg: "FullControl", "Read") and access control (eg: "Allow", "Deny")
-
-.PARAMETER INHERITANCEFLAGS
-	The attribute value which indicate permission inheritance (eg: "ContainerInherit", "ObjectInherit" or "ContainerInherit,ObjectInherit" )
-
-.PARAMETER PROPAGATIONFLAGS
-    Specifies how Access Rule are propagated to child objects.
+	Set access rule of directory path. It also allows to override and propagate all child items of this path.
 
 .EXAMPLE
-	C:\PS> Set-PathAccessRule -Path 'SomePath' -Principle "SomeUser" -Permissions @{"FullControl" = "Allow"}
+    if only one particular path needs ACL modification use the function the following way:
+    C:\PS> Set-PathAccessRule -Path 'SomePath' -Principles "someUserName" -Permissions @{"FullControl" = "Allow"}
+
+.EXAMPLE
+    if one particular path needs ACL modification, and that ACL need to be replicated into all child objects of that path then 
+    use the function the following way:
+
+    C:\PS> Set-PathAccessRule -Path 'SomePath' -Principles "someUserName" -Permissions @{"FullControl" = "Allow"} -Recurse
+
+.EXAMPLE
+    if you want to get the response of the entire opeartion then use the function the following way:
+
+    C:\PS> $response = Set-PathAccessRule -Path 'SomePath' -Principles "someUserName" -Permissions @{"FullControl" = "Allow"} -Recurse -PassThru
+    $response.TopLevelACL <- returns actual ACL object of the location specified in Path variable
+    $response.RecursedOpeartionExitCode <- returns recursion operation's (of ACL replication) exit code
+    $response.RecursedOpeartionOutput <- returns recursion operation's (of ACL replication) Standard output details
+    $response.RecursedOpeartionError <- returns recursion operation's (of ACL replication) Standard error details
 
 #>
-
 function Set-PathAccessRule {
-	param (
-		[parameter(Mandatory=$true)]
-		[String[]]
-		$Paths,
+    [CmdletBinding( PositionalBinding = $false )]
+	param 
+    (
+        # Directory path to set the access rule
+		[parameter(Mandatory=$true, ValueFromPipeline=$True)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript({ ((Get-Item $_) -is [System.IO.DirectoryInfo])  })]
+        [string]
+		$Path,
 
+        # The windows principle that the access rule applies to. Can be a string or string array
         [parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
 		[String[]]
         $Principles,
 
+        # The hashtable that contains the file system right (eg: "FullControl", "Read") and access control (eg: "Allow", "Deny")
         [parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
 		[Hashtable]
         $Permissions,
 
+        # The attribute value which indicate permission inheritance (eg: "ContainerInherit", "ObjectInherit" or "ContainerInherit,ObjectInherit" )
+        # Can be single or array of string values
         [parameter()]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet('ContainerInherit','ObjectInherit','None')]
 		[String[]]
-        $InheritanceFlags = @(”ContainerInherit", "ObjectInherit”),
+        $InheritanceFlags = @('ContainerInherit', 'ObjectInherit'),
 
+        # Specifies how Access Rule are propagated to child objects. Can be single or array of string values
         [parameter()]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet('InheritOnly', 'NoPropagateInherit', 'None')]
 		[String[]]
-        $PropagationFlags = @("None"),
+        $PropagationFlags = @('None'),
 
-        # Set if you want all child files or folders set too
+        # Instructs this method to apply all child objects to have same ACL as the top level path, specified in Path variable.
         [Switch]
-        $Recurse
+        $Recurse,
+
+        # Returns an object representing full ACL of the path. By default, this cmdlet does not generate any output.
+        [Switch]
+        $PassThru
 	)
 
-    [System.Security.AccessControl.InheritanceFlags]$InheritanceFlags = $InheritanceFlags -join ','
+     Process {
 
-    foreach ($Path in $Paths) {
+        [System.Security.AccessControl.InheritanceFlags]$InheritanceFlags = $InheritanceFlags -join ','
+        $returnValue = @{"RecursedOpeartionExitCode"=""; "RecursedOpeartionOutput"=""; "RecursedOpeartionError"=""; "TopLevelACL"=""}
+   
+        $acl = Get-Acl -Path $Path
 
-        $Acl = Get-Acl $Path
+        if (! $acl)
+        {
+            throw "acl detail of $Path is null"
+        }
 
-        foreach ($Principle in $Principles) {
+        $Principles | foreach {
 
+            $currentPrinciple = $_
+            
             foreach ($FileSystemRight in $Permissions.Keys) {
 
                 $AccessControl = $Permissions.Item($FileSystemRight)
 
-                $Ar = New-Object  system.security.accesscontrol.filesystemaccessrule($Principle,$FileSystemRight,$InheritanceFlags,$PropagationFlags,$AccessControl)
-                $Acl.SetAccessRule($Ar)
+                $Ar = New-Object  system.security.accesscontrol.filesystemaccessrule($currentPrinciple,$FileSystemRight,$InheritanceFlags,$PropagationFlags,$AccessControl)
+                $acl.SetAccessRule($Ar)
             }
 
-            Set-Acl $Path $Acl
-        }
+            Set-Acl -Path $Path -AclObject $acl
 
-        if ($Recurse) {
-            Get-ChildItem $path | foreach {
-                $parameters = @{
-                    Path = $_;
-                    $Principles = Principles;
-                    $Permissions = $Permissions;
-                    $InheritanceFlags = $InheritanceFlags;
-                    $PropagationFlags = $PropagationFlags;
-                    $Recurse = $true
+            if ($Recurse)
+            {
+
+                $arguments = """$Path\*"" /q /t /reset"
+
+                $executedProcess = Invoke-Process -ApplicationName "icacls" -Argument $arguments
+
+                if (!$executedProcess)
+                {
+                    throw "icacls returned null."
                 }
-                Set-PathAccessRule @parameters
+
+                if ($executedProcess.ExitCode -ne 0)
+                {
+                    [string] $errorMessage = $executedProcess.ErrorInfo 
+
+                    throw "icacls returned error. Error detail $errorMessage"
+                }
+
+                $returnValue.Set_Item("RecursedOpeartionExitCode", $executedProcess.ExitCode)
+                $returnValue.Set_Item("RecursedOpeartionOutput", $executedProcess.OutputInfo)
+                $returnValue.Set_Item("RecursedOpeartionError", $executedProcess.ErrorInfo)
+
             }
+
+            if ($PassThru) {
+
+                $completeACL = Get-Acl -Path $Path
+
+                $returnValue.Set_Item("TopLevelACL", $completeACL.Access)
+                
+                return $returnValue  
+            }
+    
         }
+
     }
+}
+
+<#
+
+.SYNOPSIS
+	Executes a process and returns process outputs
+
+.EXAMPLE
+    C:\PS> Invoke-Process -ApplicationName "SomeApplication.exe" -Argument "/?"
+
+.OUTPUTS
+    A hash table that contains 3 set of information. They are: ExitCode, OutputInfo, ErrorInfo
+
+#>
+function Invoke-Process {
+    [CmdletBinding( PositionalBinding = $false )]
+    [OutputType([hashtable])]
+param 
+    (
+        # The application name or application full path
+		[parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+		$ApplicationName,
+        
+        # Arguments related to the application execution
+        [string]
+		$Argument
+	)
+
+    $processInfo = New-Object System.Diagnostics.ProcessStartInfo
+    $processInfo.FileName = $ApplicationName
+    $processInfo.RedirectStandardError = $true
+    $processInfo.RedirectStandardOutput = $true
+    $processInfo.UseShellExecute = $false
+    $processInfo.Arguments = $Argument
+    $process = New-Object System.Diagnostics.Process
+    $process.StartInfo = $processInfo
+    $process.Start() | Out-Null
+    $process.WaitForExit()
+
+
+    return [hashtable]@{"ExitCode" = $process.ExitCode; "OutputInfo"=$process.StandardOutput.ReadToEnd(); "ErrorInfo"=$process.StandardError.ReadToEnd()}
+
 }
