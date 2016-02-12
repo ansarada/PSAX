deff584a99f2468bdc9296dfebd8031f319a54a4
ebh <alex.dess@ansarada.com>
2016-02-12 12:16:38 +1100

    Added Install MSI

---
+++ Functions/Install-MSI.Tests.ps1
@@ -0,0 +1,30 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Install-MSI" {
+
+    Write-Host 'Creating Fake installers for test'
+    New-item -Path $TestDrive\Realinstaller.msi -ItemType file
+    New-item -Path $TestDrive\Realinstaller.exe -ItemType file
+    
+    
+    It  'Should Throw if installer path does not exist ' {
+        {install-msi -installerpath "$TestDrive\FakeInstaller.msi" -ArgumentList 'test'} | Should Throw
+    }
+
+    It 'Should Throw if installer not an MSI' {
+        {install-msi -installerpath "$TestDrive\RealInstaller.exe" -ArgumentList 'test'} | Should Throw
+    }
+
+    It 'Should Not Throw if Installation exit code = 0' {
+        Mock -CommandName Start-Process -MockWith {return [pscustomobject]@{"exitcode" = "0"}}
+        {install-msi -installerpath "$TestDrive\RealInstaller.msi"-argumentlist 'test'} | Should not throw
+    }
+
+    It 'Should Throw if Exit code = anything but 0' {
+        Mock -CommandName Start-Process -MockWith {return [pscustomobject]@{"exitcode" = "1633"}}
+        {install-msi -installerpath 'C:\Windows' -argumentlist 'test'} | Should throw
+    }
+
+}
---
+++ Functions/Install-MSI.ps1
@@ -0,0 +1,79 @@
+﻿#requires -Version 3
+
+    <#
+            .DESCRIPTION
+            This module will help install MSI files. It will check for correct permissions, install the application and capture any exception codes.
+
+            .PARAMETER InstallerPath
+            The full path of the MSI installation file. The parameter will be checked, if the file does not exist the script will terminate. e.g. 'C:\temp\install.msi'
+
+            .PARAMETER ArgumentList	
+            All of the native MSI arguments to be passed for installation as a string array e.g. -ArgumentList @('/L*v','C:\temp\install.log','/qn','NR_LICENSE_KEY=111111111111')
+            
+            .EXAMPLE
+            Install-MSI -InstallerPath 'C:\temp\install.msi' -ArgumentList @('/L*v','C:\temp\install.log','/qn','NR_LICENSE_KEY=111111111111')
+            Will install the MSI silently with the required arguments.
+
+            .EXAMPLE
+            Install-MSI -InstallerPath 'C:\temp\install.msi' -Verbose
+            Will install the MSI with default settings and display verbose messaging.
+
+    #>
+
+Function Install-MSI
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(mandatory)]
+        [ValidateScript({
+				Test-Path -LiteralPath $_ -PathType Leaf
+        })]
+        [ValidatePattern(
+				'.msi'
+        )]
+        [String] $InstallerPath,
+
+        [Parameter(mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [String[]] $ArgumentList
+
+    )
+
+
+    Process {   
+         
+        Write-Verbose 'Checking the powershell has been run as Administrator'
+             
+        if(([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator'))
+        {
+            Write-Verbose 'Powershell running with Admin permissions. You shall pass.'
+        }
+        else 
+        {
+            Throw 'Powershell is not running with Admin permissions. Escalate permissions and try again.'
+        }
+                            
+            
+        Try 
+        {
+            Write-Verbose "Trying to install Application from $InstallerPath"
+            $InstallProcess = (Start-Process $InstallerPath -ArgumentList $ArgumentList -Wait -PassThru)
+            if ($InstallProcess.ExitCode -eq 0)
+            {
+                Write-Verbose -Message "$InstallerPath has been successfully installed according to exit code of 0"
+            }
+            else 
+            {
+                Throw "Installer exit code  $($InstallProcess.ExitCode) for file  $($InstallerPath)"
+            }
+        }
+            
+          
+        Catch 
+        {
+            Throw "Installation failed due to: $_.exception" 
+        }
+         
+    }
+}
