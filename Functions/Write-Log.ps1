573e67fc7d13eebe8368b4a9530c1c89b9b5c617
alex.dess <alex.dess@ansarada.com>
2016-07-26 08:20:36 +1000

    Revert "Revert "Revert "Revert "Adds log severity""""
    This reverts commit ba2f7ae1e492cffa0ee45c6d52b077949220ccf7.

--- Functions/Write-Log.ps1
+++ Functions/Write-Log.ps1
@@ -1,7 +1,32 @@
 Set-StrictMode -Version Latest
 
 function Write-Log {
+<#
+.SYNOPSIS
+Writes to a log file in json format Dan.
 
+.DESCRIPTION
+After calling the Start-Log function, you can use this function to write to the log file passing the log object; the actual log message; and severity.
+
+.PARAMETER Log
+The Log object returned from Start-Log.
+
+.PARAMETER LogData
+The data to be logged.
+
+.PARAMETER Severity
+Sets the severity for the log entry. Default is "Information". Valid values are: "Information", "Warning", "Error", "Debug"
+
+.EXAMPLE
+$log = Start-Log -Path C:\Temp -Name temp_log
+Write-Log -Log $log -LogData 'Hello World!' -Severity 'Debug'
+
+.NOTES
+The script will automatically log the pidId; timestamp; hostname; username and path of the file. It will write to the file in json format.
+Example of output:
+{"pidId":6508,"timeStamp":"Tuesday, 19 July 2016 2:02:24 PM","logData":"Hello World!","hostname":"dev-dantan","username":"daniel.tan","severity":"Information","logPath":"C:\\Temp\\temp_20160719_015931_6508_000.log"}
+
+#>
 	[CmdletBinding()]
 
 	param (
@@ -11,7 +36,12 @@ function Write-Log {
 
 		[parameter(mandatory=$true,position=1)]
 		[Object]
-		$LogData
+		$LogData,
+
+		[parameter(mandatory=$false)]
+		[ValidateSet("Information", "Warning", "Error", "Debug")]
+		[String]
+		$Severity = "Information"
 	)
 
 	process {
@@ -41,6 +71,7 @@ function Write-Log {
 		$pidId = [System.Diagnostics.Process]::GetCurrentProcess().Id
 
 		$LogRecord = @{
+			severity = $Severity;
 			timeStamp = $(Get-Date -Format 'F');
 			hostname = $env:ComputerName;
 			pidId = $pidId;
@@ -51,6 +82,6 @@ function Write-Log {
 		if ($Log.AddtionalLogData -ne $null) {
 				$LogRecord.Add('addtionalLogData', $Log.AddtionalLogData)
 		}
-		ConvertTo-Json $LogRecord -Compress -Depth 1000 | Out-File -FilePath $Log.LogPath -Append
+		ConvertTo-Json $LogRecord -Compress -Depth 1000 | Out-File -FilePath $Log.LogPath -Append -Encoding utf8
 	}
 }
