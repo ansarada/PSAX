4ff5d1e9813bc6ea0a8111869418e876ca9a4618
ansarada-dantan <daniel.tan@ansarada.com>
2018-02-19 15:12:55 +1100

    Added Invoke-ScriptsInFolder

---
+++ Functions/Invoke-ScriptsInFolder.Tests.ps1
@@ -0,0 +1,39 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Invoke-ScriptsInFolder" {
+
+    Write-Host 'Creating Fake scripts for test'
+    New-item -Path $TestDrive\001_test.ps1 -ItemType file
+    New-item -Path $TestDrive\002_test.ps1 -ItemType file
+    New-item -Path $TestDrive\empty -ItemType directory
+
+    It  'Should Throw if folder path does not exist ' {
+        {Invoke-ScriptsInFolder -FolderPath "$TestDrive\FakeFolder"} | Should Throw "Folder does not exist"
+    }
+
+    It 'Should Throw if no scripts in directory' {
+        {Invoke-ScriptsInFolder -FolderPath "$TestDrive\empty"} | Should Throw "No scripts found in folder"
+    }
+
+    It 'Should Throw if folder is not a folder' {
+        {Invoke-ScriptsInFolder -FolderPath "$TestDrive\001_test.ps1"} | Should Throw "FolderPath Is not a directory"
+    }
+
+    It 'Should be able to execute scripts' {
+        Mock -CommandName Start-Process { return @{exitcode = 0} }
+        Invoke-ScriptsInFolder -FolderPath "$TestDrive"
+        Assert-MockCalled Start-Process 2
+    }
+
+    It 'Should throw if script exit 1' {
+        Mock -CommandName Start-Process { return @{exitcode = 1} }
+        { Invoke-ScriptsInFolder -FolderPath "$TestDrive" } | Should Throw
+    }
+
+    It 'Should throw if child script throws' {
+        Mock -CommandName Start-Process { Throw 'random error' }
+        { Invoke-ScriptsInFolder -FolderPath "$TestDrive" } | Should Throw 'random error'
+    }
+}
---
+++ Functions/Invoke-ScriptsInFolder.ps1
@@ -0,0 +1,46 @@
+<#
+        .DESCRIPTION
+        This module will loop through and execute all scripts in a specified folder. The order will be alphanumeric
+
+        .PARAMETER FolderPath
+        The path of the folder containing the scripts
+
+        .EXAMPLE
+        Invoke-ScriptInFolder -FolderPath C:/Scripts
+#>
+Function Invoke-ScriptsInFolder
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(mandatory)]
+        [String] $FolderPath
+    )
+
+    Process {
+        write-host "Looking for scripts in $FolderPath"
+        if (!(Test-Path $FolderPath)){
+          throw "Folder does not exist"
+        }
+        if (-not ((Get-Item $FolderPath) -is [System.IO.DirectoryInfo])){
+            throw "FolderPath Is not a directory"
+        }
+
+        $scripts = ("$($FolderPath)/*.ps1" | Resolve-Path | Sort-Object)
+        if ($scripts.count -lt 1){
+            throw "No scripts found in folder"
+        }
+
+        foreach($script in $scripts){
+            Write-Host "Running script $($script.ProviderPath)";
+            $proc = (Start-Process 'powershell.exe' -ArgumentList "-File $($script.ProviderPath)" -Wait -PassThru -NoNewWindow)
+            if ($proc.exitcode -eq 0){
+              Write-Host "Script at $($script.ProviderPath) has succeeded";
+            }else{
+              throw "Non 0 return code returned. Exitcode was: $($proc.exitcode)"
+            }
+        }
+        Write-Host "All scripts in '$FolderPath' has finished executing"
+
+    }
+}
